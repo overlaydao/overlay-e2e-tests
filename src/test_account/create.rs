5e4e42a2787837f14a0cad02eb9ef795e722af5b
Hiroki Tanaka <hiroki.tanaka@kumano-te.com>
2023-04-21 07:34:03 +0900

    fix: create test account command.

--- src/test_account/create.rs
+++ src/test_account/create.rs
@@ -1,5 +1,6 @@
 use crate::{Error, Result};
 use clap::Args;
+use concordium_rust_sdk::smart_contracts::common::AccountAddress;
 use concordium_rust_sdk::{
     common::types::{KeyIndex, KeyPair, TransactionTime},
     id::{
@@ -20,11 +21,15 @@ use concordium_rust_sdk::{
             SystemAttributeRandomness, YearMonth,
         },
     },
-    types::transactions::{BlockItem, Payload},
+    types::{
+        transactions::{BlockItem, Payload},
+        CryptographicParameters,
+    },
     v2::{BlockIdentifier, Client as NodeClient, Endpoint},
 };
 use either::Either;
-use rand::thread_rng;
+use rand::{thread_rng, CryptoRng, Rng};
+use serde::Serialize;
 use std::{collections::BTreeMap, path::PathBuf, str::FromStr};
 
 #[derive(Args)]
@@ -48,6 +53,9 @@ pub struct CreateTestAccountArgs {
     )]
     anonymity_revoker: PathBuf,
 
+    #[arg(long, help = "Create initial account only flag.")]
+    initial_account: bool,
+
     #[arg(
         long,
         help = "Initial account valid_to attribute. This must be in YYYYMM format."
@@ -58,9 +66,15 @@ pub struct CreateTestAccountArgs {
     output: Option<PathBuf>,
 }
 
+#[derive(Debug, Serialize)]
+struct CreateAccountOutputs {
+    address: AccountAddress,
+    keys: AccountKeys,
+}
+
 impl CreateTestAccountArgs {
     pub async fn run(self) -> Result<()> {
-        let mut rng = thread_rng();
+        let mut csprng = thread_rng();
         let id_provider_data: IpData<IpPairing> = serde_json::from_str(
             &std::fs::read_to_string(&self.identity_provider).map_err(|_| {
                 Error::new_invalid_argument_error(format!(
@@ -90,13 +104,53 @@ impl CreateTestAccountArgs {
             ))
         })?;
 
-        let mut node_client = NodeClient::new(self.node).await?;
+        let mut node_client = NodeClient::new(self.node.clone()).await?;
         let global_context = node_client
             .get_cryptographic_parameters(&BlockIdentifier::Best)
             .await?
             .response;
+        let account = if self.initial_account {
+            self.create_init_account(
+                &mut node_client,
+                &id_provider_data,
+                &global_context,
+                &mut csprng,
+            )
+            .await?
+        } else {
+            self.create_normal_account(
+                &mut node_client,
+                &id_provider_data,
+                &anonymity_revoker_data,
+                &global_context,
+                &mut csprng,
+            )
+            .await?
+        };
 
-        let created_at = YearMonth::now();
+        // output WalletAccount json file
+        let output_file = if let Some(output) = self.output {
+            output
+        } else {
+            let mut output = std::env::current_dir().expect("must retrieve current directory...");
+            output.push("accounts");
+            output.push(format!("{}.json", account.address));
+            output
+        };
+        if let Some(parent_dir) = output_file.parent() {
+            if !parent_dir.exists() {
+                println!("output directory created: {:?}", parent_dir);
+                std::fs::create_dir_all(parent_dir)?;
+            }
+        }
+        serde_json::to_writer_pretty(
+            std::fs::File::create(output_file)?,
+            &serde_json::json!(account),
+        )?;
+        Ok(())
+    }
+
+    fn get_valid_to(&self) -> Result<YearMonth> {
         let valid_to = if let Some(year_month_str) = self.valid_to.as_deref() {
             YearMonth::from_str(year_month_str).map_err(|_| {
                 Error::new_invalid_argument_error(format!(
@@ -105,14 +159,19 @@ impl CreateTestAccountArgs {
                 ))
             })?
         } else {
+            let now = YearMonth::now();
             YearMonth {
-                year: created_at.year + 1,
-                month: created_at.month,
+                year: now.year + 1,
+                month: now.month,
             }
         };
+        Ok(valid_to)
+    }
 
-        // define initial account attributes
-        let attributes = AttributeList {
+    fn get_attributes(&self) -> Result<AttributeList<<ArCurve as Curve>::Scalar, AttributeKind>> {
+        let created_at = YearMonth::now();
+        let valid_to = self.get_valid_to()?;
+        Ok(AttributeList {
             valid_to,
             created_at,
             max_accounts: 100,
@@ -128,15 +187,26 @@ impl CreateTestAccountArgs {
             .into_iter()
             .collect(),
             _phantom: Default::default(),
-        };
+        })
+    }
+
+    async fn create_init_account<T: CryptoRng + Rng>(
+        &self,
+        node_client: &mut NodeClient,
+        id_provider_data: &IpData<IpPairing>,
+        global_context: &CryptographicParameters,
+        csprng: &mut T,
+    ) -> Result<CreateAccountOutputs> {
+        // define initial account attributes
+        let attributes = self.get_attributes()?;
         // build up initial account data
-        let init_account_credential_data = CredentialData {
-            keys: vec![(KeyIndex::from(0), KeyPair::generate(&mut rng))]
+        let credential_data = CredentialData {
+            keys: vec![(KeyIndex::from(0), KeyPair::generate(csprng))]
                 .into_iter()
                 .collect(),
             threshold: SignatureThreshold(1),
         };
-        let prf_key = SecretKey::<ArCurve>::generate_non_zero(&mut rng);
+        let prf_key = SecretKey::<ArCurve>::generate_non_zero(csprng);
         let cred_id_exponent = prf_key.prf_exponent(0).expect("We were very unlucky.");
         // RegId as well as Prf key commitments must be computed
         // with the same generators as in the commitment key.
@@ -147,16 +217,15 @@ impl CreateTestAccountArgs {
                 &Randomness::zero(),
             )
             .0;
-
         let pub_info_for_ip = PublicInformationForIp {
-            id_cred_pub: ArCurve::generate(&mut rng),
+            id_cred_pub: ArCurve::generate(csprng),
             reg_id: cred_id,
             vk_acc: CredentialPublicKeys {
-                keys: init_account_credential_data.get_public_keys(),
-                threshold: init_account_credential_data.get_threshold(),
+                keys: credential_data.get_public_keys(),
+                threshold: credential_data.get_threshold(),
             },
         };
-        let initial_account_address = account_address_from_registration_id(&pub_info_for_ip.reg_id);
+        let address = account_address_from_registration_id(&pub_info_for_ip.reg_id);
         let expiry = TransactionTime::from_seconds((chrono::Utc::now().timestamp() + 3600) as u64);
         let initial_credential_deployment_info = identity_provider::create_initial_cdi(
             &id_provider_data.public_ip_info,
@@ -165,38 +234,39 @@ impl CreateTestAccountArgs {
             expiry,
             &id_provider_data.ip_cdi_secret_key,
         );
-        let initial_account_transaction = BlockItem::<Payload>::from(AccountCredentialMessage {
+        let transaction = BlockItem::<Payload>::from(AccountCredentialMessage {
             message_expiry: expiry,
             credential: AccountCredential::Initial {
                 icdi: initial_credential_deployment_info,
             },
         });
+        println!("let's send init account transaction for {}", address);
+        let transaction_hash = node_client.send_block_item(&transaction).await?;
+        println!("transaction_hash:{}", transaction_hash);
+        let (block_hash, block_item_summary) =
+            node_client.wait_until_finalized(&transaction_hash).await?;
+        println!("Transaction finalized in block {}.", block_hash);
+        println!("The outcome is {:#?}", block_item_summary);
+        Ok(CreateAccountOutputs {
+            address,
+            keys: AccountKeys::from(credential_data),
+        })
+    }
 
-        println!(
-            "let's send init account transaction for {}",
-            initial_account_address
-        );
-        let initial_account_transaction_hash = node_client
-            .send_block_item(&initial_account_transaction)
-            .await?;
-        println!(
-            "initial_account_transaction_hash:{}",
-            initial_account_transaction_hash
-        );
-        let (initial_account_block_hash, initial_account_block_summary) = node_client
-            .wait_until_finalized(&initial_account_transaction_hash)
-            .await?;
-        println!(
-            "Transaction finalized in block {}.",
-            initial_account_block_hash
-        );
-        println!("The outcome is {:#?}", initial_account_block_summary);
-
-        // create first normal account
+    async fn create_normal_account<T: CryptoRng + Rng>(
+        &self,
+        node_client: &mut NodeClient,
+        id_provider_data: &IpData<IpPairing>,
+        anonymity_revoker_data: &ArData<ArCurve>,
+        global_context: &CryptographicParameters,
+        csprng: &mut T,
+    ) -> Result<CreateAccountOutputs> {
+        // define initial account attributes
+        let attributes = self.get_attributes()?;
         let mut ars_infos = BTreeMap::new();
         ars_infos.insert(
             anonymity_revoker_data.public_ar_info.ar_identity,
-            anonymity_revoker_data.public_ar_info,
+            anonymity_revoker_data.public_ar_info.clone(),
         );
         let ip_context = IpContext::new(
             &id_provider_data.public_ip_info,
@@ -204,8 +274,8 @@ impl CreateTestAccountArgs {
             &global_context,
         );
         let policy = Policy {
-            valid_to,
-            created_at,
+            valid_to: attributes.valid_to,
+            created_at: attributes.created_at,
             policy_vec: attributes
                 .alist
                 .iter()
@@ -214,18 +284,21 @@ impl CreateTestAccountArgs {
                 .collect(),
             _phantom: Default::default(),
         };
+        let prf_key = SecretKey::<ArCurve>::generate_non_zero(csprng);
         let aci = AccCredentialInfo {
             cred_holder_info: CredentialHolderInfo::<ArCurve> {
-                id_cred: IdCredentials::generate(&mut rng),
+                id_cred: IdCredentials::generate(csprng),
             },
             prf_key,
         };
-        let randomness = ps_sig::SigRetrievalRandomness::generate_non_zero(&mut rng);
+        let randomness = ps_sig::SigRetrievalRandomness::generate_non_zero(csprng);
         let id_use_data = IdObjectUseData { aci, randomness };
         let threshold = Threshold(1);
         let initial_account = InitialAccountData {
-            keys: init_account_credential_data.keys,
-            threshold: init_account_credential_data.threshold,
+            keys: vec![(KeyIndex::from(0), KeyPair::generate(csprng))]
+                .into_iter()
+                .collect(),
+            threshold: SignatureThreshold(1),
         };
         let (pio, _) =
             account_holder::generate_pio(&ip_context, threshold, &id_use_data, &initial_account)
@@ -245,8 +318,8 @@ impl CreateTestAccountArgs {
             alist: attributes,
             signature: ip_signature,
         };
-        let first_account_credential_data = CredentialData {
-            keys: vec![(KeyIndex::from(0), KeyPair::generate(&mut rng))]
+        let credential_data = CredentialData {
+            keys: vec![(KeyIndex::from(0), KeyPair::generate(csprng))]
                 .into_iter()
                 .collect(),
             threshold: SignatureThreshold(1),
@@ -257,60 +330,28 @@ impl CreateTestAccountArgs {
             &id_use_data,
             0,
             policy,
-            &first_account_credential_data,
+            &credential_data,
             &SystemAttributeRandomness {},
             &Either::Left(expiry),
         )?;
-        let first_account_address =
+        let address =
             account_address_from_registration_id(&credential_deployment_info.values.cred_id);
-        let first_account_transaction = BlockItem::<Payload>::from(AccountCredentialMessage {
+        let transaction = BlockItem::<Payload>::from(AccountCredentialMessage {
             message_expiry: expiry,
             credential: AccountCredential::Normal {
                 cdi: credential_deployment_info,
             },
         });
-        println!(
-            "let's send first account transaction for {}",
-            first_account_address
-        );
-        let first_account_transaction_hash = node_client
-            .send_block_item(&first_account_transaction)
-            .await?;
-        println!(
-            "first_account_transaction_hash:{}",
-            first_account_transaction_hash
-        );
-        let (first_account_block_hash, first_account_block_summary) = node_client
-            .wait_until_finalized(&first_account_transaction_hash)
-            .await?;
-        println!(
-            "Transaction finalized in block {}.",
-            first_account_block_hash
-        );
-        println!("The outcome is {:#?}", first_account_block_summary);
-
-        // output WalletAccount json file
-        let output_file = if let Some(output) = self.output {
-            output
-        } else {
-            let mut output = std::env::current_dir().expect("must retrieve current directory...");
-            output.push("accounts");
-            output.push(format!("{}.json", initial_account_address));
-            output
-        };
-        if let Some(parent_dir) = output_file.parent() {
-            if !parent_dir.exists() {
-                println!("output directory created: {:?}", parent_dir);
-                std::fs::create_dir_all(parent_dir)?;
-            }
-        }
-
-        let first_account_keys = AccountKeys::from(first_account_credential_data);
-        serde_json::to_writer_pretty(
-            std::fs::File::create(output_file)?,
-            &serde_json::json!({"address": first_account_address, "keys": first_account_keys}),
-        )?;
-
-        Ok(())
+        println!("let's send first account transaction for {}", address);
+        let transaction_hash = node_client.send_block_item(&transaction).await?;
+        println!("transaction_hash:{}", transaction_hash);
+        let (block_hash, block_item_summary) =
+            node_client.wait_until_finalized(&transaction_hash).await?;
+        println!("Transaction finalized in block {}.", block_hash);
+        println!("The outcome is {:#?}", block_item_summary);
+        Ok(CreateAccountOutputs {
+            address,
+            keys: AccountKeys::from(credential_data),
+        })
     }
 }
